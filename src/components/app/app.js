a4af6dc44d56226c39158a3719c11a30cbb65148
Safarov <elkhan.safarov10@yandex.com>
2020-09-09 14:38:20 +0500

    Экран приветствия

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import WelcomeScreen from '../welcome-screen'
+
+export default class extends Component {
+    render() {
+        return (
+            <>
+                <WelcomeScreen />
+            </>
+        )
+    }
+}
