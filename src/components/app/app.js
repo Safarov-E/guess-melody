95241b282a44c9f636aaab83d0f7b15a917dd6e0
Safarov <elkhan.safarov10@yandex.com>
2020-10-07 19:03:39 +0500

    валидации всех props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react'
 import WelcomeScreen from '../welcome-screen'
+import PropTypes from 'prop-types';
 
-export default class extends Component {
+export default class App extends Component {
     render() {
+        const {errorCount, gameTime, questions} = this.props;
         return (
             <>
-                <WelcomeScreen time={7} error={4} onClick={() => { return 1}}/>
+                <WelcomeScreen time={gameTime} error={errorCount} />
             </>
         )
     }
+}
+
+App.propTypes = {
+    errorCount: PropTypes.number.isRequired,
+    gameTime: PropTypes.number.isRequired,
+    questions: PropTypes.array.isRequired
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import questions from './mocks/questions'
 import App from './components/app'
 import './assets/styles/style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const init = (gameQuestions) => {
+    const settings = {
+        gameTime: 5,
+        errorCount: 3
+    }
+    ReactDOM.render(
+        <App 
+            errorCount={settings.errorCount}
+            gameTime={settings.gameTime}
+            questions={gameQuestions}
+        />,
+        document.getElementById('root')
+    )
+}
+init(questions)
